18c5a185d639d1284f1055b2954354deb2a92bf0
taz <tazahmedsoft@gmail.com>
2025-06-30 14:07:53 +0600

    added registration api

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.25fgudl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// mongoDb connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("event_management").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const userCollection = client.db("event_management").collection("users");
+    const eventCollection = client.db("event_management").collection("events");
+
+    app.post("/users/registration", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send({ result, message: "user added successfully" });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello from your Express app!");
+  res.send("server is connecting");
 });
 
 app.listen(port, () => {
